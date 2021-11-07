7c663cd72ced5bdc35fd33ce331f3c9b89ed8df9
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-07 14:23:23 +0700

    Add state variable to keep track of total number of waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // State variable to hold the total number of waves.
+    uint256 totalWaves;
+
     constructor() {
         console.log("WavePortal contract constructor called.");
     }
