f569818fc1c1291570b08659ee3d965c3bac0372
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-07 14:12:58 +0700

    Create script to run WavePortal contract

---
+++ scripts/run.js
@@ -0,0 +1,28 @@
+const main = async () => {
+    // This will compile the contract and generate the necessary files
+    // needed to work with the contract under the artifacts directory.
+    const waveContractFactory = await hre.ethers.getContractFactory(
+        'WavePortal',
+    );
+
+    // Hardhat will create a local Ethereum network, but just for this contract. 
+    // Then, after the script completes it'll destroy that local network.
+    const waveContract = await waveContractFactory.deploy();
+
+    // Wait until the contract is deployed to the local blockchain.
+    await waveContract.deployed();
+
+    console.log('Contract deployed to: ', waveContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
