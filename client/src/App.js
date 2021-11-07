3f993333cc4f0b58cc316e4a944b1f4a9dc3fee1
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-07 15:34:18 +0700

    Add initial structure of wave portal home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { ethers } from "ethers";
 import './App.css';
 
-function App() {
-  return (
+export default function App() {
+
+  const wave = () => {
     
+  }
+  
+  return (
+      <div className="main-container">
+          <div className="data-container">
+              <div className="header">👋 Hey there!</div>
+
+              <div className="bio">
+                  <div>Welcome to Robert's wave portal.</div>
+                  <br />
+                  <div>Connect your Ethereum wallet and wave at me!</div>
+              </div>
+
+              <button className="wave-button" onClick={wave}>
+                  Wave at Me
+              </button>
+          </div>
+      </div>
   );
 }
-
-export default App;
