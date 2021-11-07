78bf34ce9d36f8d631e4a09bf078a8772f19ac28
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-07 14:12:20 +0700

    Add constructor for WavePortal contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,5 +5,8 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    
+    constructor() {
+        console.log("WavePortal contract constructor called.");
+    }
 }
+
