cbfa78ae87e98b50f6c4cd157787349a5cf4fb49
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-07 14:15:37 +0700

    Add note about HRE

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,14 @@
 const main = async () => {
     // This will compile the contract and generate the necessary files
     // needed to work with the contract under the artifacts directory.
+    // The Hardhat Runtime Environment, or HRE, 
+    // is an object containing all the functionality that Hardhat exposes when running a task, test or script.
+    // hre is provided automatically when running 'npx hardhat...'
     const waveContractFactory = await hre.ethers.getContractFactory(
         'WavePortal',
     );
 
-    // Hardhat will create a local Ethereum network, but just for this contract. 
+    // Hardhat will create a local Ethereum network, but just for this contract.
     // Then, after the script completes it'll destroy that local network.
     const waveContract = await waveContractFactory.deploy();
 
