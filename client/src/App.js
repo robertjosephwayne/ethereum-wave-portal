bfea00d9403c732acb0c51ed46fb5b40be90f2d6
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-08 14:44:37 +0700

    Display current wave count and loading status

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import './App.css';
 import { ethers } from "ethers";
 import abi from "./utils/WavePortal.json";
 
 export default function App() {
+    const [currentWaveCount, setCurrentWaveCount] = useState(0);
     const [currentAccount, setCurrentAccount] = useState("");
+    const [loading, setLoading] = useState(true);
     const contractAddress = "0xCD3255ac7dcfD72a896a66577469590Fe36Ce48A";
     const contractABI = abi.abi;
 
@@ -61,17 +63,15 @@ export default function App() {
                 const signer = provider.getSigner();
                 const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-                let count = await wavePortalContract.getTotalWaves();
-                console.log("Retrieved total wave count: ", count.toNumber());
-
                 const waveTxn = await wavePortalContract.wave();
+                setLoading(true);
                 console.log("Mining...", waveTxn.hash);
                 
                 await waveTxn.wait();
                 console.log("Mined --", waveTxn.hash);
+                setLoading(false);
 
-                count = await wavePortalContract.getTotalWaves();
-                console.log("Retrieved total wave count: ", count.toNumber());
+                refreshCurrentWaveCount();
             } else {
                 console.log("Ethereum object doesn't exist!");
             }
@@ -80,8 +80,33 @@ export default function App() {
         }
     };
 
+    const refreshCurrentWaveCount = useCallback(async () => {
+        try {
+            const { ethereum } = window;
+
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+                setLoading(true);
+                const count = await wavePortalContract.getTotalWaves();
+                setCurrentWaveCount(count.toNumber());
+                setLoading(false);
+
+                console.log('Retrieved total wave count: ', count.toNumber());
+            } else {
+                console.log("Ethereum object doesn't exist!");
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
     useEffect(() => {
         checkIfWalletIsConnected();
+        refreshCurrentWaveCount();
     }, []);
 
     return (
@@ -104,6 +129,11 @@ export default function App() {
                         Connect Wallet
                     </button>
                 )}
+
+                {!loading && <div class="wave-count">Current Wave Count: {currentWaveCount}</div>}
+
+                {loading && <div class="loading">Loading...</div>}
+
             </div>
         </div>
     );
