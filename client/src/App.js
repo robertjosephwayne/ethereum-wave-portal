b1a2a5be4730e7696555540dab7df58e5e4e50ee
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-08 16:24:03 +0700

    Show pending and most recent transaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
-import { ethers } from "ethers";
-import abi from "./utils/WavePortal.json";
+import { ethers } from 'ethers';
+import abi from './utils/WavePortal.json';
+
+import { Button, CircularProgress } from '@mui/material';
 
 export default function App() {
     const [currentWaveCount, setCurrentWaveCount] = useState(0);
-    const [currentAccount, setCurrentAccount] = useState("");
-    const [loading, setLoading] = useState(true);
-    const contractAddress = "0xCD3255ac7dcfD72a896a66577469590Fe36Ce48A";
+    const [currentAccount, setCurrentAccount] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [pendingTransaction, setPendingTransaction] = useState('');
+    const [lastTransaction, setLastTransaction] = useState('');
+    const contractAddress = '0xCD3255ac7dcfD72a896a66577469590Fe36Ce48A';
     const contractABI = abi.abi;
 
     const checkIfWalletIsConnected = async () => {
@@ -15,20 +19,20 @@ export default function App() {
             const { ethereum } = window;
 
             if (!ethereum) {
-                console.log("Make sure you have MetaMask!");
+                console.log('Make sure you have MetaMask!');
                 return;
             } else {
-                console.log("We have the Ethereum object", ethereum);
+                console.log('We have the Ethereum object', ethereum);
             }
 
-            const accounts = await ethereum.request({ method: "eth_accounts" });
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
 
             if (accounts.length !== 0) {
                 const account = accounts[0];
-                console.log("Found an authorized account: ", account);
+                console.log('Found an authorized account: ', account);
                 setCurrentAccount(account);
             } else {
-                console.log("No authorized account found.");
+                console.log('No authorized account found.');
             }
         } catch (error) {
             console.log(error);
@@ -40,15 +44,16 @@ export default function App() {
             const { ethereum } = window;
 
             if (!ethereum) {
-                alert("Get MetaMask!");
+                alert('Get MetaMask!');
                 return;
             }
 
-            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+            const accounts = await ethereum.request({
+                method: 'eth_requestAccounts',
+            });
 
-            console.log("Connected: ", accounts[0]);
+            console.log('Connected: ', accounts[0]);
             setCurrentAccount(accounts[0]);
-
         } catch (error) {
             console.log(error);
         }
@@ -61,14 +66,19 @@ export default function App() {
             if (ethereum) {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
-                const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+                const wavePortalContract = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer,
+                );
 
                 const waveTxn = await wavePortalContract.wave();
                 setLoading(true);
-                console.log("Mining...", waveTxn.hash);
-                
+                setPendingTransaction(waveTxn.hash);
+
                 await waveTxn.wait();
-                console.log("Mined --", waveTxn.hash);
+                setPendingTransaction('');
+                setLastTransaction(waveTxn.hash);
                 setLoading(false);
 
                 refreshCurrentWaveCount();
@@ -87,27 +97,36 @@ export default function App() {
             if (ethereum) {
                 const provider = new ethers.providers.Web3Provider(ethereum);
                 const signer = provider.getSigner();
-                const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+                const wavePortalContract = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer,
+                );
 
-                setLoading(true);
                 const count = await wavePortalContract.getTotalWaves();
                 setCurrentWaveCount(count.toNumber());
-                setLoading(false);
 
                 console.log('Retrieved total wave count: ', count.toNumber());
             } else {
                 console.log("Ethereum object doesn't exist!");
             }
-
         } catch (error) {
             console.log(error);
         }
     });
 
+    const getEtherscanURL = (transaction) => {
+        return `https://rinkeby.etherscan.io/tx/${transaction}`;
+    };
+
     useEffect(() => {
         checkIfWalletIsConnected();
         refreshCurrentWaveCount();
     }, []);
+
+    useEffect(() => {
+        refreshCurrentWaveCount();
+    }, [currentAccount]);
 
     return (
         <div className="main-container">
@@ -115,25 +134,65 @@ export default function App() {
                 <div className="header">👋 Hey there!</div>
 
                 <div className="bio">
-                    <div>Welcome to Robert's wave portal.</div>
-                    <br />
-                    <div>Connect your Ethereum wallet and wave at me!</div>
+                    <div>Welcome to my wave portal.</div>
                 </div>
 
-                <button className="wave-button" onClick={wave}>
-                    Wave at Me
-                </button>
+                <div className="wave-count-container">
+                    {!loading && !!currentWaveCount && (
+                        <div>
+                            I have received {currentWaveCount} waves. I'm really
+                            popular.
+                        </div>
+                    )}
 
-                {!currentAccount && (
-                    <button className="wave-button" onClick={connectWallet}>
-                        Connect Wallet
-                    </button>
+                    {!loading && !currentAccount && (
+                        <div>Connect your Ethereum wallet and wave at me!</div>
+                    )}
+
+                    {loading && (
+                        <div>
+                            <CircularProgress className="loading-spinner" />
+                        </div>
+                    )}
+                </div>
+
+                {!loading && (
+                    <div className="button-container">
+                        {currentAccount && (
+                            <Button onClick={wave}>Wave at Me</Button>
+                        )}
+
+                        {!currentAccount && (
+                            <Button onClick={connectWallet}>
+                                Connect Wallet
+                            </Button>
+                        )}
+                    </div>
                 )}
 
-                {!loading && <div class="wave-count">Current Wave Count: {currentWaveCount}</div>}
+                <div className="transaction-container">
+                    {pendingTransaction && (
+                        <span>
+                            Your wave is processing:
+                            <a
+                                href={getEtherscanURL(pendingTransaction)}
+                                target="_blank">
+                                View on Etherscan
+                            </a>
+                        </span>
+                    )}
 
-                {loading && <div class="loading">Loading...</div>}
-
+                    {lastTransaction && (
+                        <span>
+                            Your wave has been processed:
+                            <a
+                                href={getEtherscanURL(lastTransaction)}
+                                target="_blank">
+                                View on Etherscan
+                            </a>
+                        </span>
+                    )}
+                </div>
             </div>
         </div>
     );
