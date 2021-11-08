b52bf99407e760449580ce65745c042232a75203
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-08 13:43:02 +0700

    Add wallet connection check

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import './App.css';
 
 export default function App() {
 
-  const wave = () => {
-    
-  }
-  
-  return (
-      <div className="main-container">
-          <div className="data-container">
-              <div className="header">👋 Hey there!</div>
+    const checkIfWalletIsConnected = () => {
+        const { ethereum } = window;
 
-              <div className="bio">
-                  <div>Welcome to Robert's wave portal.</div>
-                  <br />
-                  <div>Connect your Ethereum wallet and wave at me!</div>
-              </div>
+        if (!ethereum) {
+            console.log("Make sure you have MetaMask!");
+            return;
+        } else {
+            console.log("We have the Ethereum object", ethereum);
+        }
+    };
 
-              <button className="wave-button" onClick={wave}>
-                  Wave at Me
-              </button>
-          </div>
-      </div>
-  );
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    const wave = () => {
+
+    }
+
+    return (
+        <div className="main-container">
+            <div className="data-container">
+                <div className="header">👋 Hey there!</div>
+
+                <div className="bio">
+                    <div>Welcome to Robert's wave portal.</div>
+                    <br />
+                    <div>Connect your Ethereum wallet and wave at me!</div>
+                </div>
+
+                <button className="wave-button" onClick={wave}>
+                    Wave at Me
+                </button>
+            </div>
+        </div>
+    );
 }
