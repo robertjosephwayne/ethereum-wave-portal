259484376b51ff19c3d6f688f628bd20961f13e1
Robert Joseph Wayne <robertjwayne@gmail.com>
2021-11-13 15:44:20 +0700

    Create separate routes for /login and /wave

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+import Wave from './routes/wave';
+import Login from './routes/login';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/wave" element={<Wave />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    </BrowserRouter>,
+    rootElement,
+);
---
+++ client/src/routes/login.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+    Box,
+    Button,
+    Container,
+    CssBaseline,
+    Paper,
+    Typography,
+    styled,
+} from '@mui/material';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#BB86FC',
+        },
+        secondary: {
+            main: '#03DAC6',
+        },
+    },
+});
+
+export default function App() {
+    const navigate = useNavigate();
+    const [currentAccount, setCurrentAccount] = useState('');
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                console.log('Make sure you have MetaMask!');
+                return;
+            } else {
+                console.log('We have the Ethereum object', ethereum);
+            }
+
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log('Found an authorized account: ', account);
+                setCurrentAccount(account);
+                navigate('/wave');
+            } else {
+                console.log('No authorized account found.');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert('Get MetaMask!');
+                return;
+            }
+
+            const accounts = await ethereum.request({
+                method: 'eth_requestAccounts',
+            });
+
+            console.log('Connected: ', accounts[0]);
+            setCurrentAccount(accounts[0]);
+            navigate('/wave');
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 10,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                    textAlign="center">
+                    <Typography component="h1" variant="h4">
+                        Welcome to Robert's wave portal.
+                    </Typography>
+                    <Paper sx={{ paddingX: 6, paddingY: 4, margin: 5 }}>
+                        {!currentAccount && (
+                            <Typography component="h1" variant="h6">
+                                Connect your wallet to enter.
+                            </Typography>
+                        )}
+                        <Box noValidate sx={{ my: 2 }}>
+                            {!currentAccount && (
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    onClick={connectWallet}
+                                    size="large"
+                                    color="secondary">
+                                    Connect
+                                </Button>
+                            )}
+                        </Box>
+                    </Paper>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
